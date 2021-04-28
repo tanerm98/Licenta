f2c9495c0f5c5763496a5d4499c8af907e4bb6d9
Taner Mustafa <tmustafa@fitbit.com>
2021-04-28 18:52:06 +0300

    Unzip

--- BusinessLogic/utils.py
+++ BusinessLogic/utils.py
@@ -1,14 +1,18 @@
 #!/usr/local/bin/python3
 
+
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaIoBaseDownload
 from google.oauth2 import service_account
 import io
 import json
 import subprocess
+import zipfile
+import os
 
 import logging
 logging.getLogger().setLevel(logging.INFO)
+
 
 def download_file_from_google_drive(file_id, file_name):
     """
@@ -36,14 +40,35 @@ def download_file_from_google_drive(file_id, file_name):
             logging.info("Status: %d%% downloaded..." % int(status.progress() * 100))
 
     except Exception as e:
-        logging.error("Download failed with error {ERROR}".format(ERROR=e))
+        logging.error("Download failed with error '{ERROR}'".format(ERROR=e))
         return False
 
     logging.info("Download successful!")
     return True
 
 
-def comment_on_PR(auth_token, comment, repo_owner, repo_name, pr_number):
+def unzip_archive(archive_name):
+    """
+    Unzips archive with APP file.
+    :param archive_name: name of archive downloaded from Google Drive.
+    :return: APP file name if successful; None if failure.
+    Usage: unzip_archive("app.zip")
+    """
+    try:
+        logging.info("Extracting archive '{ARCHIVE}'...".format(ARCHIVE=archive_name))
+        with zipfile.ZipFile(archive_name, 'r') as zip_ref:
+            zip_ref.extractall(os.getcwd())
+            app_name = str(zip_ref.namelist()[0])[:-1]
+
+    except Exception as e:
+        logging.error("Extracting failed with error '{ERROR}'".format(ERROR=e))
+        return None
+
+    logging.info("Extracted app name: '{NAME}'".format(NAME=app_name))
+    return app_name
+
+
+def comment_on_pr(auth_token, comment, repo_owner, repo_name, pr_number):
     """
     Posts comment on a pull-request in a Github repository
     :param auth_token: authentication token for the Github account
@@ -53,10 +78,10 @@ def comment_on_PR(auth_token, comment, repo_owner, repo_name, pr_number):
     :param pr_number: pull-request ID
     :return: True - if comment successful; False - if comment failed.
 
-    Usage: comment_on_PR("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", "Test de comment 1", "tanerm98", "Licenta", 1)
+    Usage: comment_on_pr("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", "Test de comment 1", "tanerm98", "Licenta", 1)
     """
     try:
-        logging.info("Posting comment {COMMENT} on PR {PR} for repository {REPO}...".format(
+        logging.info("Posting comment '{COMMENT}' on PR '{PR}' for repository '{REPO}'...".format(
             COMMENT=comment,
             PR=pr_number,
             REPO=repo_name)
@@ -74,7 +99,7 @@ def comment_on_PR(auth_token, comment, repo_owner, repo_name, pr_number):
         subprocess.Popen(command, shell=True).wait()
 
     except Exception as e:
-        logging.error("Commenting failed with error {ERROR}".format(ERROR=e))
+        logging.error("Commenting failed with error '{ERROR}'".format(ERROR=e))
         return False
 
     logging.info("Comment posted successfuly!")
